b3d3fdf559c760b82516d3888d1713db64bb5c6d
NatoBentz <juniorbzaguila@hotmail.com>
2018-03-12 18:39:46 -0600

    se agrega forms a entradas

---
+++ practica_1/entradasForm2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica_1
+{
+    public partial class entradasForm2 : Form
+    {
+        public entradasForm2()
+        {
+            InitializeComponent();
+        }
+    }
+}
